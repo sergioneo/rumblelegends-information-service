142f54e3af9841d50d05ee8836557d0e02ecf447
Sergio Yanez <sergioy@amazon.com>
2018-09-03 23:54:25 -0700

    Pagination

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,11 +1,30 @@
 class QueryController < ApplicationController
   def get
 
+  	number_per_page = params["limit"]
+  	offset = params["offset"]
+  	order_by = params["orderBy"]
+  	start = params["start"]
+  	asc = params["asc"]
+  	comp = "<="
+  	if asc = "ASC"
+  		comp = ">="
+  	end
+
   	result_payload = Hash.new
 
-  	query = Beast.all.order(:external_id)
+  	puts order_by
+  	puts comp
+  	puts start
 
-  	result_payload["found"] = query.count
+  	query = Beast.where(order_by+" "+comp+" "+start)
+  				 .limit(number_per_page)
+  				 .offset(offset)
+  				 .order(order_by+" "+asc)
+
+  	big_query = Beast.where(order_by+" "+comp+" "+start)
+
+  	result_payload["found"] = big_query.count
   	result_payload["hits"] = query
 
   	render json: result_payload
